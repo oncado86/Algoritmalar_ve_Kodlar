f1aca1486a63fff64cd4e2904d6fccf350f13e12
oncado86 <77399565+oncado86@users.noreply.github.com>
2021-05-22 17:42:36 +0300

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+hello = ["Hello everyone, I'm OnCaDo.",
+         "I am studying in Software Engineering and sharing what I know with you.",
+         "May the Source Code be with you!"]
+
+for i in hello:
+    print(i)
